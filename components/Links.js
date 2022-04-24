87eb16c6e47179cb76fdfe424088226e176b44a9
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2022-04-24 12:15:27 +0044

    Update Links.js

--- components/Links.js
+++ components/Links.js
@@ -4,7 +4,7 @@ export default function LinkTree(){
         <>
             <div className="links-area">
 
-                <div className="text-center area-btn-links"><Link href="/brussels" className="btn-links">Conheça a Brussels</Link></div>
+                <div className="text-center area-btn-links"><Link href="/brussels" className="btn-links"><a>Conheça a Brussels</a></Link></div>
                 <div className="text-center area-btn-links"><a href="#" className="btn-links text-center">Conheça a Estrela Sirius</a></div>
                 <div className="text-center area-btn-links"><a href="#" className="btn-links">Conheça o Chopp Egypt</a></div>
                 <div className="text-center area-btn-links"><a href="#" className="btn-links">Peça no Zé Delivery</a></div>
