e451ddc64f39b7f7b34fe4a5d3dac617b6ad337f
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2022-04-24 21:04:35 +0044

    Update
    Página da Brussels..

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../assets/styles/globals.css'
+import '../assets/styles/brussels.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/brussels.js
+++ pages/brussels.js
@@ -1,32 +1,58 @@
 import Image from "next/image"
+import Cabecalho from "../components/Cabecalho";
 
 export default function Brussels(){
     return(
         <>
+            <Cabecalho />
+            <h1 className="text-center mt-5 brussels-h1">A Cerveja Brussels</h1>
             <div className="brussels-body md:flex flex-row gap-5">
-                <div className="brussels-img-area">
-                    <Image src="/img/Heineken.jpg" width={700} height={500} />
+                <div className="brussels-area">
+                    <div className="brussels-block">
+                        <div className="brussels-img-area-1">
+                            <Image src="/img/pg_brussels/brussels01.jpg" width={300} height={300} />
+                        </div>
+                        <div className="brussels-txt">
+                            <h2 className="brussels-h2">Excelência no processo produtivo</h2>
+                            <p className="brussels-p">
+                                A grande aceitação no mercado brasileiro é fruto da 
+                                seleção criteriosa das matérias primas e do rigoroso 
+                                padrão de qualidade na produção. Em nosso laboratório, 
+                                são inspecionados mais de 15 itens antes da cerveja 
+                                ir para distribuição.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="brussels-block-2">
+                        <div className="brussels-img-area-2">
+                            <Image src="/img/pg_brussels/brussels02.jpg" width={300} height={300} />
+                        </div>
+                        <div className="brussels-txt">
+                            <h2 className="brussels-h2">A essência da nossa fórmula</h2>
+                            <p className="brussels-p">
+                                Os cuidados em todas as etapas do processo 
+                                produtivo são fundamentais para manter viva 
+                                a essência de nossa fórmula: uma Puro Malte 
+                                de corpo leve, bem atenuada e com baixo amargor.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="brussels-block">
+                        <div className="brussels-img-area-3">
+                            <Image src="/img/pg_brussels/brussels03.jpg" width={300} height={300} />
+                        </div>
+                        <div className="brussels-txt">
+                            <h2 className="brussels-h2">A sua próxima paixão</h2>
+                            <p className="brussels-p">
+                                A perfeita combinação entre talento e sabor! 
+                                Experimente e surpreenda-se.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div className="brussels-txt-area">
-                    <div>
-                        <h2>Excelência no processo produtivo</h2>
-                        <p>
-                            A grande aceitação no mercado brasileiro é fruto da seleção criteriosa das matérias primas e do rigoroso padrão de qualidade na produção. Em nosso laboratório, são inspecionados mais de 15 itens antes da cerveja ir para distribuição.
-                        </p>
-                    </div>
-                    <div>
-                        <h2>A essência da nossa fórmula</h2>
-                        <p>
-                            Os cuidados em todas as etapas do processo produtivo são fundamentais para manter viva a essência de nossa fórmula: uma Puro Malte de corpo leve, bem atenuada e com baixo amargor.
-                        </p>
-                    </div>
-                    <div>
-                        <h2>A sua próxima paixão</h2>
-                        <p>
-                            A perfeita combinação entre talento e sabor! Experimente e surpreenda-se.
-                        </p>
-                    </div>
-                </div>
+            </div>
+            <div className="brussels-logo-egypt">
+                <Image src="/img/logo-transparente.png" width={150} height={150} />
             </div>
         </>
     )
