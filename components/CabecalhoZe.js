02acb720916597ce9dbb76eac96d1936166ccb53
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2022-04-24 13:51:18 +0044

    Create CabecalhoZe.js

---
+++ components/CabecalhoZe.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Cabecalho(){
+    return(
+        <>
+            <Head>
+                <title>Egypt Beer</title>
+                <meta name="author" content="Marcos Henrique Marcarini Junior"/>
+                <meta name="theme-color" content="#facc15">
+            </Head>
+        </>
+    )
+}
